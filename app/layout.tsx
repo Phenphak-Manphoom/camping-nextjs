ed6ecd0e8939f5509dfff267dc4b128a03bda00e
Tae <phenphak.forlearn@gmail.com>
2025-08-22 14:07:52 +0700

    feat: add user profile with dropdown menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/์Navbar/Navbar";
-import Providers from "./providers";
+import Navbar from "@/components/Navbar/Navbar";
+import Providers from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ utils/links.ts
@@ -0,0 +1,9 @@
+type NavLink = {
+  href: string;
+  label: string;
+};
+export const links: NavLink[] = [
+  { href: "/", label: "Home" },
+  { href: "/favorite", label: "Favorite" },
+  { href: "/camp", label: "Camp" },
+];
