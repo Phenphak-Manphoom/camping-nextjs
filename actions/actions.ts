27637da2f243060f9e8365971b49adf2317d81ba
Tae <phenphak.forlearn@gmail.com>
2025-09-04 14:02:11 +0700

    feat:create profile

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,19 @@
 "use server";
 
 import { profileSchema, validateWithZod } from "@/utils/schemas";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import db from "@/utils/db";
+import { redirect } from "next/navigation";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in");
+  }
+
+  if (!user.privateMetadata.hasProfile) redirect("/create-profile");
+  return user;
+};
 
 const renderError = (error: unknown): { message: string } => {
   return {
@@ -13,13 +26,30 @@ export const createProfileAction = async (
   formData: FormData
 ) => {
   try {
+    const user = await getAuthUser();
+
     const rawData = Object.fromEntries(formData);
     const validatedData = validateWithZod(profileSchema, rawData);
-    console.log("validated", validatedData);
+    //console.log("validated", validatedData);
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedData,
+      },
+    });
 
-    return { message: "Profile created successfully!" };
+    const client = await clerkClient();
+    await client.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+    // return { message: "Profile created successfully!" };
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return renderError(error);
   }
+  redirect("/");
 };
