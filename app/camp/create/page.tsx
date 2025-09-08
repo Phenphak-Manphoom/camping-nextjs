269129b3de3d460117cc0296cc6631bcc0130fd0
Tae <phenphak.forlearn@gmail.com>
2025-09-08 16:21:04 +0700

    feat: add category feature with dropdown menu using shadcn

--- app/camp/create/page.tsx
+++ app/camp/create/page.tsx
@@ -1,5 +1,6 @@
 import { createLandmarkAction } from "@/actions/actions";
 import { SubmitButton } from "@/components/form/Buttons";
+import CategoryInput from "@/components/form/CategoryInput";
 import FormContainer from "@/components/form/FormContainer";
 import FormInput from "@/components/form/FormInput";
 import { currentUser } from "@clerk/nextjs/server";
@@ -15,14 +16,18 @@ const CreateLandmark = async () => {
       </h1>
       <div className="border p-8 rounded-md ">
         <FormContainer action={createLandmarkAction}>
-          <div className="grid md:grid-cols-2 gap-4 mt-4">
+         <div className="grid md:grid-cols-2 gap-4 mt-4">
             <FormInput
               name="name"
               label="Landmark Name"
               type="text"
               placeholder="Landmark Name"
             />
+
+            {/* Category */}
+            <CategoryInput />
           </div>
+
           <SubmitButton text="create landmark" size="lg" />
         </FormContainer>
       </div>
